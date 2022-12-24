a03d0054a2dd47bcc5124e54804e924716a968cb
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-12-25 01:51:44 +0530

    Create home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="container-h">
+          <h1>usedefi</h1>
+        </div>
+      </div>
+      <div className="content">
+        <div className="container-c">
+          <ul>
+            <p>Level 1 Beginners Track</p>
+            <li>What is DeFi?</li>
+            <li>What are Stable Coins?</li>
+            <li>What is Liquidity Pools?</li>
+            <li>What is Staking?</li>
+            <li>What is Liquidity Mining</li>
+            <li>What is Automated Market Maker (AMM)?</li>
+            <li>What is Bridges?</li>
+            <li>What is Mining or Farming?</li>
+            <li>What is Impermanent Loss or Divergent Loss?</li>
+            <li>What is APY?</li>
+          </ul>
+        </div>
+        <div className="container-c">
+          <ul>
+            <p>Level 2 Decentralised Lending Protocol</p>
+            <li>Compound</li>
+            <li>Aave</li>
+          </ul>
+        </div>
+        <div className="container-c">
+          <ul>
+            <p>Level 3 Decentralised Exchange (DEX) or AMM</p>
+            <li>Uniswap</li>
+            <li>Pancake swap</li>
+          </ul>
+        </div>
+        <div className="container-c">
+          <ul>
+            <p>Level 4 Decentralised Yield Optimization</p>
+            <li>Yearn Finance</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="container-f">
+          <h1>build by anonymous</h1>
+        </div>
+      </div>
     </div>
   );
 }
