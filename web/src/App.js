ff7efcdbb7a79e892637b4adc98cce458ff74a65
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-12-25 22:53:48 +0530

    Add Context Component

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Content from './component/content';
 
 function App() {
   return (
@@ -8,46 +9,11 @@ function App() {
           <h1>usedefi</h1>
         </div>
       </div>
-      <div className="content">
-        <div className="container-c">
-          <ul>
-            <p>Level 1 Beginners Track</p>
-            <li>What is DeFi?</li>
-            <li>What are Stable Coins?</li>
-            <li>What is Liquidity Pools?</li>
-            <li>What is Staking?</li>
-            <li>What is Liquidity Mining</li>
-            <li>What is Automated Market Maker (AMM)?</li>
-            <li>What is Bridges?</li>
-            <li>What is Mining or Farming?</li>
-            <li>What is Impermanent Loss or Divergent Loss?</li>
-            <li>What is APY?</li>
-          </ul>
-        </div>
-        <div className="container-c">
-          <ul>
-            <p>Level 2 Decentralised Lending Protocol</p>
-            <li>Compound</li>
-            <li>Aave</li>
-          </ul>
-        </div>
-        <div className="container-c">
-          <ul>
-            <p>Level 3 Decentralised Exchange (DEX) or AMM</p>
-            <li>Uniswap</li>
-            <li>Pancake swap</li>
-          </ul>
-        </div>
-        <div className="container-c">
-          <ul>
-            <p>Level 4 Decentralised Yield Optimization</p>
-            <li>Yearn Finance</li>
-          </ul>
-        </div>
-      </div>
+      <Content />
       <div className="footer">
         <div className="container-f">
-          <h1>build by anonymous</h1>
+          <h1>build with ❤️ for you</h1>
+          <h1>keep learning and keep building</h1>
         </div>
       </div>
     </div>
---
+++ web/src/component/content.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+const Content = () => {
+    return (
+        <div className="content">
+            <div className="container-c">
+                <ul>
+                    <p>Level 1 Beginners Track</p>
+                    <li>What is DeFi?</li>
+                    <li>What are Stable Coins?</li>
+                    <li>What is Liquidity Pools?</li>
+                    <li>What is Staking?</li>
+                    <li>What is Liquidity Mining</li>
+                    <li>What is Automated Market Maker (AMM)?</li>
+                    <li>What is Bridges?</li>
+                    <li>What is Mining or Farming?</li>
+                    <li>What is Impermanent Loss or Divergent Loss?</li>
+                    <li>What is APY?</li>
+                </ul>
+            </div>
+            <div className="container-c">
+                <ul>
+                    <p>Level 2 Decentralised Lending Protocol</p>
+                    <li>Compound</li>
+                    <li>Aave</li>
+                </ul>
+            </div>
+            <div className="container-c">
+                <ul>
+                    <p>Level 3 Decentralised Exchange (DEX) or AMM</p>
+                    <li>Uniswap</li>
+                    <li>Pancake swap</li>
+                </ul>
+            </div>
+            <div className="container-c">
+                <ul>
+                    <p>Level 4 Decentralised Yield Optimization</p>
+                    <li>Yearn Finance</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Content
