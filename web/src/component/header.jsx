a3858061abead4af102a431e7068a6232fb52e45
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-12-25 22:59:11 +0530

    Create Header Component

---
+++ web/src/component/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="container-h">
+                <h1>usedefi</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
