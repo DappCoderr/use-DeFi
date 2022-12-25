507d272c10874ad70c6a63c39c27c25ac132e961
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2022-12-25 22:59:22 +0530

    Create Footer Component

---
+++ web/src/component/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="container-f">
+                <h1>build with ❤️ for you</h1>
+                <h1>keep learning and keep building</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
